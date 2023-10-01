56214ef7f0de1515383a4ed021663b272ad96f29
Juanseproo <143037261+Juanseproo@users.noreply.github.com>
2023-09-30 21:21:00 -0500

    Update TextLibSimul.py
    Corrección código anteriormente subido

--- TextLibSimul.py
+++ TextLibSimul.py
@@ -3,10 +3,10 @@ import unittest
 class TestSimulaciones(unittest.TestCase):
 
     def test_Cal_Prob_Posi(self):
-        Cal_Prob_Posi = c.Cal_Prob_Posi([2-1j, 2j, 1-1j, 1-2j, 2], (1-1j))
-        self.assertAlmostEqual(Cal_Prob_Posi, 0.050000000000000044)
-        Cal_Prob_Posi = c.Cal_Prob_Posi([2-1j, 2j, 1-1j, 1-2j, 3], (1-1j))
-        self.assertAlmostEqual(Cal_Prob_Posi, 0.040000000000000036) 
+        Cal_Prob_Posi = c.Cal_Prob_Posi([-3-1j, -2j, 1j, 2], 2)
+        self.assertAlmostEqual(Cal_Prob_Posi, 5.263157894736841)
+        Cal_Prob_Posi = c.Cal_Prob_Posi([2+1j, -1+2j, 1j, 1, 3-1j, 2, -2j, -2+1j, 1-3j, -1j], 7)
+        self.assertAlmostEqual(Cal_Prob_Posi, 10.869565217391303) 
 
 
 if __name__ == "__main__":
