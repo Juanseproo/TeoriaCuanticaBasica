c7210cdd012892cfb1d0d16d4c6d227cea3e2457
Juanseproo <143037261+Juanseproo@users.noreply.github.com>
2023-10-06 11:54:29 -0500

    Update TextLibSimul.py
    Pruebas para la nueva funcion amplitud_de_transicion

--- TextLibSimul.py
+++ TextLibSimul.py
@@ -10,10 +10,15 @@ class TestSimulaciones(unittest.TestCase):
     
     def test_Cal_Prob_Posi_Doble(self):
         Cal_Prob_Posi_Doble = c.Cal_Prob_Posi_Doble([2+1j, -1+2j, 1j, 1, 3-1j, 2, -2j, -2+1j, 1-3j, -1j], [-3-1j, -2j, 1j, 2, -3-1j, -3-1j, -3-1j, -3-1j, -3-1j, -3-1j])
-        self.assertAlmostEqual(Cal_Prob_Posi_Doble, (-0.2985932897257321+0.1824736770546141j))
-        Cal_Prob_Posi_Doble = c.Cal_Prob_Posi_Doble([1j, -1+2j, 1j, 1, 3-1j, 2, -2j, -2+1j, 1-3j, -1j], [-3-1j, -2j, 1j, 2, -3-1j, -3-1j, -3-1j, -3-1j, -3-1j, -3-1j])
-        self.assertAlmostEqual(Cal_Prob_Posi_Doble, (-0.2083257985376444+0.2256862817491148j))
+        self.assertAlmostEqual(Cal_Prob_Posi_Doble, (-0.23223922534223615+0.31518180582160615j))
+        Cal_Prob_Posi_Doble = c.Cal_Prob_Posi_Doble([1j, -1+2j, 1j, 1, 3-1j, 2, -2j, -2+1j, 1-3j, -1j], [-1-4j, 2-3j, -7+6j, -1+1j, -5-3j, 5, 5+8j, 4-4j, -8-7j, -2-7j])
+        self.assertAlmostEqual(Cal_Prob_Posi_Doble, (-0.12190844191195455+0.17927712045875666j))
 
+    def test_amplitud_de_transicion(self):
+        amplitud_de_transicion = c.amplitud_de_transicion([1j, 1], [1, -1j])
+        self.assertAlmostEqual(amplitud_de_transicion, (-0.9999999999999998j))
+        amplitud_de_transicion = c.amplitud_de_transicion([2j, 5], [2, -1j])
+        self.assertAlmostEqual(amplitud_de_transicion, (-0.7474093186836597j))
 
 if __name__ == "__main__":
     unittest.main()
