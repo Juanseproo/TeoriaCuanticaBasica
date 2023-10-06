6743bcb33af271fe98ed8f745a1536417e0fce48
Juanseproo <143037261+Juanseproo@users.noreply.github.com>
2023-10-06 11:53:45 -0500

    Update Simulaciones.py
    Nueva función que calcula Amplitud de transición entre 2 vectores dados

--- Simulaciones.py
+++ Simulaciones.py
@@ -1,16 +1,5 @@
 import math
 import sys
-def Cal_Prob_Posi(vector, posicion, denominador = 0):
-    """
-    El sistema debe calcular la probabilidad de encontrarlo en una posición en particular.
-    (list2d, int) -> float 
-    """
-    for i in range(len(vector)):
-        denominador += math.sqrt(vector[i].real**2 + vector[i].imag**2)**2
-    denominador, numerador = math.sqrt(denominador), vector[posicion]
-    result = (math.sqrt(numerador.real**2 + numerador.imag**2)**2/denominador**2)*100
-    return result
-
 def normalizar_vector(vector, suma = 0):
     """
     Normaliza un vector dado
@@ -28,13 +17,24 @@ def Producto_int_2_vectores(A, B):
         print("Deben tener ambos la misma longuitud")
         sys.exit()
     return sum(v1 * v2 for v1, v2 in zip(A, B))
-    
+
 def conjugar_vector(vector):
     """
     Conjunga un vector dado
     """
     conjugar = [complex(x.real, -x.imag) for x in vector]
     return conjugar
+
+def Cal_Prob_Posi(vector, posicion, denominador = 0):
+    """
+    El sistema debe calcular la probabilidad de encontrarlo en una posición en particular.
+    (list2d, int) -> float 
+    """
+    for i in range(len(vector)):
+        denominador += math.sqrt(vector[i].real**2 + vector[i].imag**2)**2
+    denominador, numerador = math.sqrt(denominador), vector[posicion]
+    result = (math.sqrt(numerador.real**2 + numerador.imag**2)**2/denominador**2)*100
+    return result
 
 def Cal_Prob_Posi_Doble(vector1, vector2):
     """
@@ -48,4 +48,13 @@ def Cal_Prob_Posi_Doble(vector1, vector2):
     for i in range(len(vector1)):
         vector1[i], vector2[i] = vector1[i]/N_vector1, vector2[i]/N_vector2
     result = Producto_int_2_vectores(vector1, vector2)
+    return result
+
+def amplitud_de_transicion(vector1, vector2):
+    """
+    Amplitud de transición. El sistema puede recibir dos vectores y calcular la probabilidad de transitar de el uno al otro después de hacer la observación
+    """
+    numerador = Producto_int_2_vectores(conjugar_vector(vector1), vector2)
+    denominador = normalizar_vector(vector1) * normalizar_vector(vector2)
+    result = numerador/denominador
     return result
